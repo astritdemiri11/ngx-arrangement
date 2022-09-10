23c963fabe64a423297d0f8a966d956b24669063
astritdemiri11 <astritdemiri11@gmail.com>
2022-09-10 23:45:51 +0200

    Readme file fixes

--- src/public-api.ts
+++ src/public-api.ts
@@ -3,6 +3,7 @@
  */
 
 export { ArrangementModule } from './lib/ngx-arrangement.module';
+
 export { LayoutService } from './lib/services/layout/layout.service';
 export { ServerSideDirective } from './lib/directives/server-side/server-side.directive';
 export { ServerSideRenderDirective } from './lib/directives/server-side-render/server-side-render.directive';
